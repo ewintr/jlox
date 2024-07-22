1e78c8c7f324df3a5bc1992ac68d4277613c45b0
Erik Winter <erik@deonticdata.com>
2024-07-22 14:30:33 +0200

    4.2

--- src/Lox.java
+++ src/Lox.java
@@ -4,9 +4,12 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -21,6 +24,9 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) {
+            System.exit(65);
+        }
     }
 
     private static void runPrompt() throws IOException {
@@ -32,6 +38,7 @@ public class Lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+            hadError = false;
         }
     }
 
@@ -41,7 +48,16 @@ public class Lox {
 
         for (Token token : tokens) {
             System.out.println(token);
-            
+
         }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[" + line + "] " + where + ": " + message);
+        hadError = true;
     }
 }
